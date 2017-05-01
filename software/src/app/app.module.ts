99821c1ad2c3cf35b8b95e9ff2048e1ea4217590
Check <farid.m.check@gmail.com>
2017-05-01 09:45:56 +0200

    Add the imports and providers.

--- software/src/app/app.module.ts
+++ software/src/app/app.module.ts
@@ -1,28 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AuthorFormComponent } from './Components/Author/author-form/author-form.component';
-import { AuthorListComponent } from './Components/Author/author-list/author-list.component';
-import { BookListComponent } from './Components/Book/book-list/book-list.component';
-import { BookFormComponent } from './Components/Book/book-form/book-form.component';
 import { NavbarComponent } from './Components/Nav/navbar/navbar.component';
+import { AuthorListComponent } from './Components/Author/author-list/author-list.component';
+import { AuthorFormComponent } from './Components/Author/author-form/author-form.component';
+import { BookFormComponent } from './Components/Book/book-form/book-form.component';
+import { BookListComponent } from './Components/Book/book-list/book-list.component';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthorFormComponent,
+    NavbarComponent,
     AuthorListComponent,
-    BookListComponent,
+    AuthorFormComponent,
     BookFormComponent,
-    NavbarComponent
+    BookListComponent,
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    ReactiveFormsModule,
+    HttpModule,
+    FlashMessagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
