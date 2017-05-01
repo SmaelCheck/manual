9953e03d7789c41b915ae1235cda44ba1d7f0692
Check <farid.m.check@gmail.com>
2017-05-01 19:31:50 +0200

    Improvement the register function adding isObject, instanceCreate and isValid functions.

--- software/src/app/Services/Book/book.service.ts
+++ software/src/app/Services/Book/book.service.ts
@@ -14,12 +14,10 @@ export class BookService {
   ) { }
 
   register(book: any){
-    if(book.title.length < 0 || book.author.name.length < 0 || book.pub_date.length < 0){
-        // Create a book instance or object to save these data;
-        let abook: Book;
-        abook.title     = book.title;
-        abook.author    = book.author;
-        abook.pub_date  = book.pub_date;
+     // Test if this variable is object
+    if(this._isObject(book)){
+        // Create a book instance to save these data;
+        let abook = this._instanceCreate(book);
 
         // If the data were successfully saved.
         if(abook.save()){
@@ -29,19 +27,40 @@ export class BookService {
         }
         // Else i display an error message.
         else this._flashMessagesService.show('Error : please fill correctly the fields.',
-                {cssClass:'alert-danger',timeout:10000});
+            {cssClass:'alert-danger',timeout:10000});
+    }
+    // Variable isn't an object
+    else{
+        this._flashMessagesService.show('Erreur : veuillez renseigner correctement ces champs en respectant ces propres formats.',
+            {cssClass:'alert-danger',timeout:6000}
+        );
     }
   }
-
-  delete(book: Book){
-
+  private _isObject(obj:any): Boolean{
+      return typeof obj === 'object' ? true: false ;
+  }
+  private _isValid(book:any): Boolean{
+      if(book.title.length <= 0 || book.author.name.length <= 0 || book.pub_date.length <= 0){
+          return false;
+      }
+      return true;
   }
 
-  getBooks(){
-
+    /**
+     *
+     * @param book
+     * @returns {Book}
+     * @private
+     */
+  private _instanceCreate(book:any):Book{
+      let abook: Book;
+      abook.title     = book.title;
+      abook.author    = book.author;
+      abook.pub_date  = book.pub_date;
+      // return a book instance
+      return abook;
   }
-
-  update(book: Book){
-
-  }
+  delete(book: Book){}
+  getBooks()/*: Book[]*/{}
+  update(book: Book){}
 }
