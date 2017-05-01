c6e8c8496a0af79ae98827ee6f1fff342cb46254
Check <farid.m.check@gmail.com>
2017-05-01 09:42:45 +0200

    Book the features.

--- software/src/app/Components/Book/book-form/book-form.component.ts
+++ software/src/app/Components/Book/book-form/book-form.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import{FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
+// declarate
+declare  var jQuery:any;
 @Component({
-  selector: 'app-book-form',
+  selector: 'book-form',
   templateUrl: './book-form.component.html',
   styleUrls: ['./book-form.component.scss']
 })
 export class BookFormComponent implements OnInit {
+  book_form: FormGroup;
 
-  constructor() { }
+  constructor(private _fb: FormBuilder, private _fs: FlashMessagesService) { }
+
 
   ngOnInit() {
+
+    this.book_form = this._fb.group({
+      title:['',[Validators.required, Validators.minLength(5),Validators.maxLength(50)]],
+      author:['',[Validators.required, Validators.minLength(5),Validators.maxLength(50)]],
+      pub_date:['',Validators.required]
+    });
+  }
+
+  onSubmit(){
+
+    this._fs.show('Data send successful',{ cssClass:' alert-danger', timeout:15000});
+
+    // const book ={
+    //   title: this.book_form.get('title').value,
+    //   author: this.book_form.get('author').value,
+    //   pub_date: this.book_form.get('pub_date').value
+    // };
   }
 
 }
