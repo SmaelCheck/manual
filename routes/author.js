ca705d914849d638321da14f068006ed2f70ed8f
Check <farid.m.check@gmail.com>
2017-05-01 08:17:57 +0200

    The routes features.

--- routes/author.js
+++ routes/author.js
@@ -1,3 +1,11 @@
 /**
  * Created by smael on 01/05/2017.
  */
+let express = require('express'),
+    router  = express.Router();
+
+router.get('/author',(req,res,next)=>{
+    res.send('Get all authors');
+});
+
+module.exports = router;
--- routes/book.js
+++ routes/book.js
@@ -1,3 +1,37 @@
 /**
  * Created by smael on 01/05/2017.
  */
+
+let express = require('express'),
+    router  = express.Router();
+
+router.get('/book',(req,res,next)=>{
+    res.send('Get all books.');
+});
+
+
+router.get('/book/add',(req,res,next)=>{
+    res.send('Add a book.');
+});
+
+router.get('/book:id',(req,res,next)=>{
+    res.send('get a book.');
+});
+
+router.post('/book/register',(req,res,next)=>{
+    res.send('Add a book.');
+});
+
+router.post('/book',(req,res,next)=>{
+    res.send('Post a book.');
+});
+
+router.put('/book:id',(req,res,next)=>{
+    res.send('Update a book.');
+});
+
+router.delete('/book:id',(req,res,next)=>{
+    res.send('Delete a book.');
+});
+
+module.exports = router;
