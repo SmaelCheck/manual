629fd5bffd381ddad664eb76825b3b2f364ba3af
Check <farid.m.check@gmail.com>
2017-05-01 09:47:16 +0200

    Add the features into services.

--- software/src/app/Services/Book/book.service.ts
+++ software/src/app/Services/Book/book.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import {Book} from '../../Models/Book/book';
+
 
 @Injectable()
 export class BookService {
 
-  constructor() { }
+  // The Book Collection
+  private _books: Book[];
 
+  constructor( private _flashMessagesService: FlashMessagesService) { }
+
+  register(book: Book){
+
+  }
+
+  delete(book: Book){
+
+  }
+
+  getBooks(){
+
+  }
+
+  update(book: Book){
+
+  }
 }
